9b0b1a95da19f4aa7593c18384198dc0f13c4ead
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-07 16:09:44 +0100

    Task 7

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const config = {
     apiKey: "AIzaSyDDOwPtv46O8gP0f-RBsvT9nDp3yrRXxtQ",
     authDomain: "ad-snadbox.firebaseapp.com",
@@ -5,6 +7,7 @@ const config = {
     projectId: "ad-snadbox",
     storageBucket: "ad-snadbox.appspot.com",
     messagingSenderId: "328854615659"
-  }
+}
 
- firebase.initializeApp(config)
+const firebaseApp = firebase.initializeApp(config)
+export const database = firebaseApp.database()
