69dfecdb59bb55ea75828fb2b21ad5d0f5539171
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-07 10:21:01 +0100

    add Task Description

--- src/Counter.js
+++ src/Counter.js
@@ -1,3 +1,15 @@
+// 1. Zrób komponent Counter - 2 przyciski i h1 wyświetlający aktualny stan Countera
+// 1.2. Załącz Counter do App
+// 2. Niech komponent Counter wyświetla początkowo wartość przekazaną w propsie `startValue`
+// 2.2 Niech komponent Counter ma ustawioną domyślną wartość propsa `startValue` na 3
+// 2.3 Niech istnienie propsa `startValue` będzie wymagane i wymagane przekazanie w nim liczby. Skorzystaj z biblioteki `prop-types`
+
+// https://reactjs.org/docs/typechecking-with-proptypes.html (edited)
+
+
+
+
+
 import React from 'react'
 import PropTypes from 'prop-types'
 
