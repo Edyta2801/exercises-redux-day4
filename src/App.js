d541e5199c6a469456a1dccfd7660e064cb77337
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-07 11:41:31 +0100

    Task 4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 import Counter from './Counter'
 import PassingProps from './PassingProps'
 
-
 const App = (props) => (
   <div>
-    <Counter
-      startValue={0}
-    />
-
-    <Counter
-   />
-   <PassingProps
-      passingValue={5}
-      passingFun={() => alert('bu')}
-    />
+    <Router>
+      <div>
+        <Route path='/' exact component={Counter} />
+        <Route path='/counter' component={Counter} />
+        {/* PassingProps component with no props */}
+        <Route path='/passing-props' component={PassingProps} />
+      </div>
+    </Router>
   </div>
 )
 
